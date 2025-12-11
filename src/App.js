bfa5d6d36982d8b55c8b977f9c36d58ffd3cd4f2
kirankulal97 <kiran.kulal@technomers.com>
2025-12-11 15:32:30 +0530

    deployment 6

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>CICD Deployment 5</h1>
+        <h1>CICD Deployment 6</h1>
       </header>
     </div>
   );

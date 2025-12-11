5e5b3c6c8ade454d716f1f1d9b87008c154ed915
kirankulal97 <kiran.kulal@technomers.com>
2025-12-11 14:35:33 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hello</h1>
+        <h1>hello welcom</h1>
       </header>
     </div>
   );

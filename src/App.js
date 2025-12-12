d4892b3a0d8f74a2b490f94e135b7a5a8a4aaf87
kiran kulal <kirankulal37@gmail.com>
2025-12-12 11:17:56 +0530

    CI/CD new Deploymnt 3

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> CI/CD + Sonar + Vercel dployment </h1>
+        <h1> CI/CD + Sonar + Vercel dployment 3 </h1>
 
       </header>
     </div>

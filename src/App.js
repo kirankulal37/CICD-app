1491a910d37ccb7ae00d666a2d2a8917ec81a013
kiran kulal <kirankulal37@gmail.com>
2025-12-12 12:54:49 +0530

    deployment

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-// // import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <h1>CI/CD + Sonar + Vercel deployment 3</h1>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 // import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  // 🚨 SonarCloud test errors:
-  const password = "12345"; // hardcoded password
-  eval("console.log('test')"); // unsafe eval usage
-
   return (
     <div className="App">
       <header className="App-header">
@@ -33,3 +12,24 @@ function App() {
 }
 
 export default App;
+
+
+// // import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+
+//   // 🚨 SonarCloud test errors:
+//   const password = "12345"; // hardcoded password
+//   eval("console.log('test')"); // unsafe eval usage
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>CI/CD + Sonar + Vercel deployment 3</h1>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;

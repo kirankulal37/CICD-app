fc7965cb0fe322bac0f09518fca967a4e946b9b4
kiran kulal <kirankulal37@gmail.com>
2025-12-14 21:56:33 +0530

    deoployment 10

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>CI/CD + Sonar + Vercel deployment 9</h1>
+        <h1>CI/CD + Sonar + Vercel deployment 10</h1>
       </header>
     </div>
   );
